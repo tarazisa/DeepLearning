af7726fde0de61b413e627ac98ab1ab14c3025af
tarazisa <tarazisa@gmail.com>
2017-06-14 16:17:49 -0400

    finished forward propagation

--- backpropagation_01.py
+++ backpropagation_01.py
@@ -5,21 +5,13 @@ import math
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 
-input_layer_dim = 2
-hidden_layer_nodes = 2
-output_layer_dim = 1
-
-hidden_layers_nodes = np.array([2])
 input_values = np.array([0, 3])
 layers = np.array([2, 3, 3, 2])
-layers_values = {'0': [0, 3]}
-output_layer = np.array([0])
-
-
+layers_values = {}
 weights = {}
 
-
 def init_layers_values():
+    #default all values to 0, do not forget to assign input
     for i in range(layers.size):
         values = np.array([])
         values.resize(layers[i],)
@@ -27,34 +19,52 @@ def init_layers_values():
 
 
 def init_weights():
-    start = -1/math.sqrt(layers[0])
-    end = 1/math.sqrt(layers[0])
-    # Weights from input to first hidden layer
-    #wl = []
-    #for i in range(input_layer.size):
-    #    w = []
-    #    for layer_size in range(hidden_layers[0]):
-    #            w.append(rnd.uniform(start, end))
-    #    wl.append(w)
-    #weights['i'] = wl
 
-    # Weights from hidden layers up to output
+    # Layer 0 is input
     for layer_num in range((layers.size - 1)):
+        start = -1 / math.sqrt(layers[layer_num])
+        end = 1 / math.sqrt(layers[layer_num])
+
         from_layer_size = layers[layer_num]
         to_layer_size = layers[layer_num + 1]
         wl = []
-        for node in range(from_layer_size):
+        for i in range(to_layer_size):
             w = []
-            for i in range(to_layer_size):
+            for node in range(from_layer_size):
                 w.append(rnd.uniform(start, end))
             wl.append(w)
         weights[str(layer_num)+"->"+str(layer_num+1)] = wl
 
 
+def relu(value):
+    return max(0., value)
+
+
+def forward_propagate():
+    for layer_num in range(layers.size - 1):
+        output_values = []
+        values = np.array(layers_values[str(layer_num)])
+        w = weights[str(layer_num)+"->"+str(layer_num+1)]
+        for node in range(len(w)):
+            print ("Now processing weights: %s %s " % (values, w[node]))
+            output_value = (values * w[node]).sum()
+            output_value = relu(output_value)
+            print(output_value)
+            output_values.append(output_value)
+        layers_values[str(layer_num+1)] = output_values
+        print(output_values)
+        print(layers_values)
+
+
 # predict_with_network(input_layer, weights)
 init_layers_values()
 init_weights()
-#init Input Values
+print (layers_values)
+print(weights)
+
+
+# init Input Values
 layers_values['0'] = input_values
+forward_propagate()
 print (layers_values)
 print(weights)
